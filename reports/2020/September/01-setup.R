053133829e0ceef02273509585dcded56cd40a14
Demetri Pananos <dpananos@gmail.com>
2020-04-07 16:01:57 -0400

    removed reports

--- reports/2020/September/01-setup.R
+++
@@ -1,9 +0,0 @@
-# Create POEM Report for September 2020
-
-
-poemReport::data_cleaning(report_month = "September",
-                            report_year = "2020")
-
-poemReport::poem_report(report_month = "September",
-                            report_year = "2020",
-                            render = FALSE)
